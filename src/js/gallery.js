b00979446346496bedf152a5c8c97b4eb5e20beb
R3enox <nikr3enox@gmail.com>
2023-09-16 13:59:23 +0300

    do task

---
+++ src/js/gallery.js
@@ -0,0 +1,75 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { refs } from './refs';
+import { PixabayAPI } from './pixabay-api';
+import galerryCard from '../templates/gallery-cards.hbs';
+import { lightbox } from './simpleLightbox';
+
+const pixabayApi = new PixabayAPI(40);
+
+let totalPages = 0;
+
+refs.form.addEventListener('submit', onSubmit);
+
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    if (entries[0].isIntersecting) {
+      loadMoreData();
+    }
+  },
+  {
+    root: null,
+    rootMargin: '300px',
+    threshold: 1,
+  }
+);
+
+async function onSubmit(evt) {
+  evt.preventDefault();
+
+  const searchQ = evt.currentTarget.elements['searchQuery'].value.trim();
+
+  if (!searchQ) {
+    return Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+
+  pixabayApi.q = searchQ;
+  pixabayApi.page = 1;
+  try {
+    const { totalHits, hits } = await pixabayApi.getPhotos();
+
+    totalPages = Math.ceil(totalHits / 40);
+
+    Notify.success(`Hooray! We found ${totalHits} images.`);
+    refs.list.innerHTML = galerryCard(hits);
+    lightbox.refresh();
+
+    if (totalPages === 1) {
+      return;
+    }
+    observer.observe(refs.guard);
+  } catch (error) {
+    Notify.failure(`${error}`);
+  } finally {
+    refs.form.reset();
+  }
+}
+
+async function loadMoreData() {
+  pixabayApi.page += 1;
+
+  const { hits } = await pixabayApi.getPhotos();
+
+  try {
+    if (pixabayApi.page === totalPages) {
+      Notify.info("We're sorry, but you've reached the end of search results.");
+      observer.unobserve(refs.guard);
+      return;
+    }
+    refs.list.insertAdjacentHTML('beforeend', galerryCard(hits));
+    lightbox.refresh();
+  } catch {
+    Notify.failure(`${error}`);
+  }
+}
---
+++ src/js/refs.js
@@ -0,0 +1,5 @@
+export const refs = {
+  form: document.getElementById('search-form'),
+  list: document.querySelector('.js-gallery'),
+  guard: document.querySelector('.js-guard'),
+};
